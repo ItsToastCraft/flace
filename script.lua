a18c8feea4e6ded704de74581d48ec01b240bb5d
ItsToastCraft <itstoastcraft@gmail.com>
2024-04-05 17:15:04 -0400

    Updates
    * Added a new hat provided by @proto_narrator
    * Fixed some hats not being shown on the list
    * Added a new home page in case others want to add different sections
    * Added the ability to remove hats

--- script.lua
+++ script.lua
@@ -7,6 +7,7 @@ vanilla_model.HELMET:setVisible(false)
 vanilla_model.CAPE:setVisible(false)
 vanilla_model.HELMET_ITEM:setVisible(true)
 
+-- 4P5's deepCopy function
 local function deepCopy(model)
 	local copy = model:copy(model:getName())
 	for _, child in pairs(copy:getChildren()) do
@@ -14,9 +15,9 @@ local function deepCopy(model)
 	end
 	return copy
 end
+
 --variables --
-ping = {}
-local lhat = 0
+local hat = 0
 -- some model stuff --
 local model = models.model
 local head = model.Head
@@ -27,7 +28,6 @@ head.Head:setPrimaryTexture("SKIN")
 head.Head:setPrimaryRenderType("CUTOUT_CULL")
 head.Het:setScale(1.4, 1.4, 1.4)
 
-
 local skull = deepCopy(head)
 skull:removeChild(skull.Head)
 skull:setParentType("None")
@@ -36,6 +36,7 @@ model.Skull:addChild(skull)
 
 -- hats --
 local hats = {
+	{ head.None,        "None",         skull.None,        "#973131" },
 	{ head.FarmerHat,   "Farmer Hat",   skull.FarmerHat,   "#d8af3c" },
 	{ head.Goggles,     "Goggles",      skull.Goggles,     "#ffe792" },
 	{ head.BaseballCap, "Baseball Cap", skull.BaseballCap, "#d4362b" },
@@ -45,10 +46,12 @@ local hats = {
 	{ head.GNHat,       "GN's Hat",     skull.GNHat,       "#3b932a" },
 	{ head.WitchHat,    "Witch Hat",    skull.WitchHat,    "#8f27d1" },
 	{ head.Het,         "Het",          skull.Het,         "#a247b8 " },
+	{ head.CRT,         "CRT Monitor",  skull.CRT,         "#696a6a " }
 }
 
 local currentHat = config:load("hat") or 0
 
+-- pings -- 
 function pings.setHat(x)
 	x = x or 0
 	currentHat = x
@@ -60,52 +63,63 @@ end
 
 pings.setHat(currentHat)
 
--- pings --
-
-A = function(x) print(x) end
-
-local pages = { action_wheel:newPage("hat1") }
-local index = 1
-action_wheel:setPage(pages[index])
 
 -- action wheel --
+
+local pages = { action_wheel:newPage("hat") }
+local buttonColor = "#3764b1"
+
+local main = action_wheel:newPage("main")
+main:newAction()
+	:setTitle("Hats")
+	:setItem("leather_helmet{display:{color:9908529}}")
+	:setColor(vectors.hexToRGB("#973131"))
+	:setOnLeftClick(function() action_wheel:setPage(pages[1]) end)
+action_wheel:setPage(main)
+
+
 for i = 1, #hats do
-	if i % 9 == 1 then
-		table.insert(pages, action_wheel:newPage("hat" .. i % 9 + 1))
+	if i % 7 == 0 then -- Adjusted the condition to start a new page
+		table.insert(pages, action_wheel:newPage("hat"))
 	end
 
-	pages[math.floor(i / 7) + 1]:newAction(1)
-		:setTexture(textures.texture, 112, 0, 16, 16, 1)
-		:setTitle("Next Page")
-		:setColor(vectors.hexToRGB("#3764b1"))
-		:setOnLeftClick(function()
-			action_wheel:setPage(pages[(index % #pages) + 1])
-			index = index + 1
-		end)
+	local pageIndex = math.floor(i / 7) + 1
 
-	pages[math.floor(i / 7) + 1]:newAction((i % 7) + 1)
+	local slot = ((i - 1) % 6) + 2
+	if pageIndex == 1 then
+		pages[1]:newAction(8)
+			:setTexture(textures.texture, 112, 16, 16, 16, 1)
+			:setTitle("Home")
+			:setColor(vectors.hexToRGB(buttonColor))
+			:setOnLeftClick(function()
+				action_wheel:setPage(main)
+			end)
+	else
+		pages[pageIndex]:newAction(8)
+			:setTexture(textures.texture, 96, 0, 16, 16, 1)
+			:setTitle("Previous Page")
+			:setColor(vectors.hexToRGB(buttonColor))
+			:setOnLeftClick(function()
+				action_wheel:setPage(pages[((pageIndex - 2) % #pages) + 1])
+			end)
+	end
+	pages[pageIndex]:newAction(slot)
 		:setItem("player_head{SkullOwner:" .. "ItsToastCraft" .. ",Data:" .. i .. "}")
 		:setTitle(hats[i][2])
 		:setOnLeftClick(function() pings.setHat(i) end)
 		:setColor(vectors.hexToRGB(hats[i][4]))
-	pages[math.floor(i / 7) + 1]:newAction(8)
-
-		:setTexture(textures.texture, 96, 0, 16, 16, 1)
-		:setTitle("Previous Page")
-		:setColor(vectors.hexToRGB("#3764b1"))
+	pages[pageIndex]:newAction(1)
+		:setTexture(textures.texture, 112, 0, 16, 16, 1)
+		:setTitle("Next Page")
+		:setColor(vectors.hexToRGB(buttonColor))
 		:setOnLeftClick(function()
-			action_wheel:setPage(pages[(index % #pages) + 1])
-			index = index - 1
+			action_wheel:setPage(pages[(pageIndex % #pages) + 1])
 		end)
 end
 
-
 function events.skull_render(delta, blockstate, itemStack, entity, type)
 	if itemStack ~= nil then
-		data = itemStack["tag"]["Data"] or currentHat
-		if data == nil then
-			data = currentHat
-		end
+		local data = itemStack["tag"]["Data"] or currentHat
 		for i, v in pairs(hats) do
 			v[3]:setVisible(data == i)
 		end
@@ -115,16 +129,11 @@ function events.skull_render(delta, blockstate, itemStack, entity, type)
 end
 
 --   tick   --
-function events.render()
+function events.tick()
 	--variables--
-
 	local worldTime = world.getTime()
 
 	if worldTime % 200 == 0 then
 		pings.setHat(currentHat)
-	end
-
-	if lhat ~= currentHat then
-		lhat = currentHat
 	end
 end
